937636f9caa0da9cf6a2d48d583cdafbddf95648
Zxilly <zhouxinyu1001@gmail.com>
2022-01-09 21:11:08 +0800

    chore: remove comment

--- webpack.production.js
+++ webpack.production.js
@@ -2,7 +2,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-// noinspection JSUnresolvedFunction,SpellCheckingInspection
 module.exports = merge(common, {
   devtool: false,
   mode: 'production',
