a2e1891269db5d55458aae0008a443de82c1f205
Zxilly <zhouxinyu1001@gmail.com>
2022-02-23 19:57:15 +0800

    feat: add missing file
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

---
+++ api/api.ts
@@ -0,0 +1,17 @@
+import { createConnection } from 'typeorm';
+import { generate } from '../src/main';
+
+const iServer = generate();
+const { server, connection, token } = iServer;
+console.log(`Server ready at ${process.env.VERCEL_URL}`);
+console.log(`Token: ${token}`);
+createConnection(connection)
+  .then(() => {
+    console.warn('Database connected');
+  })
+  .catch((err) => {
+    console.error('Database connection error: ', err);
+    process.exit(1);
+  });
+
+module.exports = server;
